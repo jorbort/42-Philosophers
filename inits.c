811ae93380d2535227b5de411a3728c151835dba
Jorge Bortolotti Suarez <jbortolo@car7s3.42barcelona.com>
2023-10-04 15:53:11 +0200

    arreglada la creacion de mutexes

--- inits.c
+++ inits.c
@@ -6,7 +6,7 @@
 /*   By: jbortolo <jbortolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 09:58:03 by jbortolo          #+#    #+#             */
-/*   Updated: 2023/10/04 15:20:15 by jbortolo         ###   ########.fr       */
+/*   Updated: 2023/10/04 15:50:57 by jbortolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,13 @@ void	init_struct(t_program *program)
 
 void	init_forks(t_program *program)
 {
+	int	i;
+
+	i = 0;
 	program->fork = malloc(sizeof(pthread_mutex_t) * program->n_of_philos);
+	while (i < program->n_of_philos)
+	{
+		pthread_mutex_init(&program->fork[i], NULL);
+		i++;
+	}
 }
--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: jbortolo <jbortolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 15:10:35 by jbortolo          #+#    #+#             */
-/*   Updated: 2023/10/04 15:41:18 by jbortolo         ###   ########.fr       */
+/*   Updated: 2023/10/04 15:51:01 by jbortolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	routine(t_program *program)
 {
-	pthread_mutex_init(&program->fork, NULL);
 	while (program->flag == false || program->n_of_meals != 0)
 	{
 		
