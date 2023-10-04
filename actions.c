e66036bfbb23c27fc3173cf9766a8d1abbb02bc7
Jorge Bortolotti Suarez <jbortolo@car7s3.42barcelona.com>
2023-10-04 16:25:49 +0200

    agregados  mas funciones de actions

--- actions.c
+++ actions.c
@@ -6,23 +6,48 @@
 /*   By: jbortolo <jbortolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 15:58:45 by jbortolo          #+#    #+#             */
-/*   Updated: 2023/10/04 16:13:03 by jbortolo         ###   ########.fr       */
+/*   Updated: 2023/10/04 16:24:47 by jbortolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	eat(t_program *program)
+// locks two forks to eat
+void	eat(t_program *program, int index)
 {
-	if (i == program->n_of_philos)
+	if (index == program->n_of_philos)
 	{
-		pthread_mutex_lock(&program->fork[i]);
-		pthread_mutex_lock(&program->fork[i - 1]);
+		pthread_mutex_lock(&program->fork[index]);
+		pthread_mutex_lock(&program->fork[index - 1]);
 	}
 	else
 	{
-		pthread_mutex_lock(&program->fork[i]);
-		pthread_mutex_lock(&program->fork[i + 1]);
+		pthread_mutex_lock(&program->fork[index]);
+		pthread_mutex_lock(&program->fork[index + 1]);
 	}
-	printf("philosopher %d is eating \n", program->philos[i]);
+	printf("philosopher %d is eating \n", index);
+	usleep(program->t_to_eat);
+	program->n_of_meals--;
+}
+
+void	think(t_program *program, int index)
+{
+	if (index == program->n_of_philos)
+	{
+		pthread_mutex_unlock(&program->fork[index]);
+		pthread_mutex_unlock(&program->fork[index - 1]);
+	}
+	else
+	{
+		pthread_mutex_unlock(&program->fork[index]);
+		pthread_mutex_unlock(&program->fork[index + 1]);
+	}
+	printf("philosopher %d is thinking\n", index);
+}
+
+//set a usleep of the time the philosopher must sleep
+void	sleeping(t_program *program, int index)
+{
+	usleep(program->t_to_sleep);
+	printf("philosopher %d is sleeping\n",index);
 }
